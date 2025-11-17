d1e9c2170af4da0f647ca1406057854bcd188e38
Samii Shabuse <sshabuse@gmail.com>
2025-11-16 20:10:37 -0500

    feature(drexel_colors): adding file for drexel colors to be referenced

---
+++ app/composeApp/src/commonMain/kotlin/com/example/demo/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.demo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Drexel University Colors
+val DrexelBlue = Color(0xFF07294D)   // #07294D
+val DrexelGold = Color(0xFFFFC600)   // #FFC600
+val FieldBackground = Color(0xFF073B70)
+val HintGrey = Color(0xFF9AA4B2)
